9c62f4220cc0c25e5b8dce3313f10e4d1573228a
pranjay2109 <pranjaygupta11@gmail.com>
2022-07-20 00:08:50 +0530

    Added Speech Recognition to code
    Added Speech Recognition to code google vs sphinx

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    audio = r.listen(source)
+    text = r.recognize_google(audio)
+    print(text)
+
